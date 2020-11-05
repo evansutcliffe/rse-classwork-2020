0b42b2cd1e75cb1177871e1a78a352cdc2e46689
Evan Sutcliffe <30503110+evansutcliffe@users.noreply.github.com>
2020-11-05 11:24:02 +0000

    added additional tests rse-classwork-2020/issues/ #86

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -8,3 +8,33 @@ def test_given_input():
     result = times.compute_overlap_time(large, short)
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected
+
+
+def test_no_overlap():
+    large = times.time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00")
+    short = times.time_range("2010-01-13 12:00:00", "2010-01-13 13:00:00")
+    #print(compute_overlap_time(large, short))
+    result = times.compute_overlap_time(large, short)
+    print(result)
+    expected = []
+    assert result == expected   
+
+def test_multiple_intervals():
+    interval1 = times.time_range("2010-01-12 10:00:00", "2010-01-12 10:45:00", 2, 60)
+    interval2 = times.time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    #print(interval1)
+    #print(interval2)
+    #print(times.compute_overlap_time(interval1, interval2))
+    result = times.compute_overlap_time(interval1, interval2)
+    expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+    assert result == expected
+
+# def test_exact_intervals():
+#     interval1 = times.time_range("2010-01-12 10:00:00", "2010-01-12 10:45:00")
+#     interval2 = times.time_range("2010-01-12 10:45:00", "2010-01-12 11:00:00")
+#     #print(interval1)
+#     #print(interval2)
+#     #print(times.compute_overlap_time(interval1, interval2))
+#     result = times.compute_overlap_time(interval1, interval2)
+#     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+#     assert result == expected
