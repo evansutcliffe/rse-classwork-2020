9e190726e2492356febd9e694959e3a639caa84c
Evan Sutcliffe <30503110+evansutcliffe@users.noreply.github.com>
2020-11-05 12:46:11 +0000

    updated negative and param

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -2,23 +2,40 @@
 import times
 # content of test_expectation.py
 import pytest
+from pytest import raises
+#import raise
 
 
 @pytest.mark.parametrize("test_input,expected", [
-    (["2010-01-12 10:00:00", "2010-01-12 12:00:00","2010-01-13 12:00:00", "2010-01-13 13:00:00"], []),
-    (["2010-01-12 10:00:00", "2010-01-12 12:00:00","2010-01-13 12:00:00", "2010-01-13 13:00:00"], [])
+    ([times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"),
+    times.time_range("2010-01-13 12:00:00", "2010-01-13 13:00:00")], []),
+
+    ([times.time_range("2010-01-12 10:00:00", "2010-01-12 10:45:00", 2, 60),
+    times.time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)], 
+    [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]),
+
+    ([times.time_range("2010-01-12 10:00:00", "2010-01-12 10:45:00"),
+    times.time_range("2010-01-12 10:45:00", "2010-01-12 11:00:00")], 
+    []),
     ])
+
 def test_eval(test_input, expected):
     assert eval(test_input) == expected
 
 
 def eval(input):
-    interval1 = times.time_range(input[0], input[1])
-    interval2 = times.time_range(input[2], input[3])
-    result = times.compute_overlap_time(interval1, interval2)
+    result = times.compute_overlap_time(input[0], input[1])
     return result
 
+def test_negative_input():
+    with raises(ValueError) as err:
+        times.time_range("2010-01-12 12:00:00", "2010-01-12 10:00:00")
+        err.match("Start time after end time")
 
+
+    #result = times.compute_overlap_time(large, short)
+    #expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+    #assert result == expected
 
 # @pytest.mark.parametrize("test_input,expected", 
 # ("times.time_range('2010-01-12 10:00:00', '2010-01-12 11:00:00'), times.time_range('2010-01-13 12:00:00', '2010-01-13 13:00:00')"
--- week05-testing/times.py
+++ week05-testing/times.py
@@ -5,6 +5,10 @@ def time_range(start_time, end_time, number_of_intervals=1, gap_between_interval
     start_time_s = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
     end_time_s = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
     d = (end_time_s - start_time_s).total_seconds() / number_of_intervals + gap_between_intervals_s * (1 / number_of_intervals - 1)
+    
+    if (end_time_s<start_time_s):
+        raise ValueError("Start time after end time")
+
     sec_range = [(start_time_s + datetime.timedelta(seconds=i * d + i * gap_between_intervals_s),
                   start_time_s + datetime.timedelta(seconds=(i + 1) * d + i * gap_between_intervals_s))
                  for i in range(number_of_intervals)]
